8197b6b500e587ecf845f4bcbe87ac9e77d391f3
Peter Kvačkay <peter.kvackay@gmail.com>
2017-03-10 20:42:37 +0100

    Implementovany command points

--- hraci.py
+++ hraci.py
@@ -1,8 +1,26 @@
-class Hrac:
-	junior=False
+import cmd
 
+zoznam={'fero': 4}	
+
+class Hrac:
+	
+	junior=False
+	
 	def __init__(self,name,points):
 		self.name=name
 		self.points=points
+
+class ParseCommands(cmd.Cmd):
+	
+	def do_points(self,args):
+		name,points=args.split()
+		zoznam[name]+=int(points)
+		print(name,zoznam[name])
+ 
+	
+	
+
 password=input('Put your new password: ')
+
+ParseCommands().cmdloop()
 
